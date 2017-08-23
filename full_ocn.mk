6d1335bab07e8f7e0f596f7ad7e84a873f9757e4
Tab Fitts <tfitts1212@gmail.com>
2017-08-23 13:21:12 -0700

    [device/ocn] Fusionify makefiles.

--- full_ocn.mk
+++ full_ocn.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright 2017 Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,14 +13,39 @@
 # limitations under the License.
 #
 
-# Inherit from ocn device
-$(call inherit-product, device/htc/ocn/device.mk)
+IS_ARM64 := true
 
+# Include Official OTA Package
+WITH_OFFICIALOTA := true
+
+# Support Beta OTA Opt-in (Requires additional ota xml)
+BETA_OTA := true
+
+# Include pure telephony configuration
+$(call inherit-product, vendor/pure/configs/pure_phone.mk)
+
+# Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := ocn 
-PRODUCT_NAME := full_ocn
+# Inherit from ocn device
+$(call inherit-product, device/htc/ocn/device.mk)
+
+
+# Set those variables here to overwrite the inherited values.
+
+PRODUCT_DEVICE := ocn
+PRODUCT_NAME := ocn
+PRODUCT_BRAND := HTC
+PRODUCT_MODEL := ocn
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_RELEASE_NAME := ocn
+
+TARGET_VENDOR := htc
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
 
 $(call inherit-product-if-exists, vendor/htc/ocn/ocn-vendor.mk)
